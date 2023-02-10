509fb308bc1735e4eade8ebc3014b651f4f452af
kguers <kguers@live.esu.edu>
2023-02-10 15:01:36 -0500

    Nav component

--- src/app/App.js
+++ src/app/App.js
@@ -1,12 +1,20 @@
 import React from "react";
 import store from './store';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
 import { Provider } from "react-redux";
+import { Navigation } from "../components/navigation/navigation";
 
 export const App = () => {
-  
+
   return (
     <Provider store={store}>
-      
+      <Container fluid>
+        <Row>
+          <Navigation />
+        </Row>
+      </Container>
     </Provider>
   );
 }
---
+++ src/components/navigation/navigation.js
@@ -0,0 +1,58 @@
+import React, { useState } from "react";
+import { Container, Navbar, Nav, Form, Button } from "react-bootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faRightToBracket, faPersonRunning, faCartShopping } from '@fortawesome/free-solid-svg-icons';
+import './navigation.css'
+
+export const Navigation = () => {
+
+    const [sign, setSign] = useState(false)
+
+    return (
+        <Navbar variant="pills" className="navbar">
+            <Container fluid>
+                <Navbar.Brand className="logo ms-4">
+                    <img
+                        className='logo-img'
+                        src='./justenough.png'
+                        alt='Just Enough Logo'
+                    />
+                </Navbar.Brand>
+                <Form className="d-flex">
+                    <Form.Control
+                        type="search"
+                        placeholder="Search"
+                        className="me-2"
+                        aria-label="Search"
+                    />
+                    <Button variant="primary">Search</Button>
+                </Form>
+                <Nav>
+                    <Button 
+                        onMouseEnter={() => setSign(true)}
+                        onMouseLeave={() => setSign(false)}
+                        className="me-2 navRightButtons"
+                    >
+                        {!sign
+                            ?
+                            <FontAwesomeIcon
+                                icon={faRightToBracket}
+                            />
+                            :
+                            <FontAwesomeIcon
+                                icon={faPersonRunning}
+                            />
+                        }
+                    </Button>
+                    <Button 
+                        className="me-2 navRightButtons"
+                    >
+                        <FontAwesomeIcon 
+                            icon={faCartShopping}
+                        />
+                    </Button>
+                </Nav>
+            </Container>
+        </Navbar>
+    );
+}
