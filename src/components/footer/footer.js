b3518a0c310e9c0acaba1beea1821aaf75cecfb7
kguers <kguers@live.esu.edu>
2023-02-16 17:02:10 -0500

    mobile footer

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -7,8 +7,8 @@ export const Footer = () => {
     return (
         <Row className="footer accountForNav">
             <Col className="text-start align-self-center">
-            <Image className="d-inline logo-footer" src="./images/justenough-logo.png" alt="Logo without wording" />        
-            <p className="d-inline p-2 text-nowrap">&copy; Just Enough Podcast, LLC 2023 </p> 
+            <Image className="d-none d-sm-inline logo-footer" src="./images/justenough-logo.png" alt="Logo without wording" />        
+            <p className=" d-inline p-2 text-nowrap">&copy; Just Enough Podcast, LLC 2023 </p> 
             </Col>
             <Col className="align-self-center text-end col-3">
                 <a href="https://github.com/kguers/shop.justenough.github.io" target="_blank" rel="noopener noreferrer">
